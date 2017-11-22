ee4b9a3307ee5cf0c63970b15f68c5ac34d7efef
YangJian1992 <1556492839@qq.com>
2017-11-22 15:34:16 +0800

    不熟练，还得学一学pandas

--- call_history_addr_alter/call_addr_alter.py
+++ call_history_addr_alter/call_addr_alter.py
@@ -1,6 +1,6 @@
 #coding: utf-8
 import re
-# import os
+import os
 # import sys
 import time
 import numpy as np
@@ -78,12 +78,72 @@ group by call_addr
 def read_local_data():
     path = "D:\\work\\database\\province-city\\"
     file_name = "province_city_rule.json"
-    data = pd.read_table(path + file_name, encoding='utf-8', sep='\t')
-    #增加新列"call_addr_new"
-    data['call_addr_new'] = "NULL"
-    print(data)
-    return data
+    file_name_2 = 'call_addr_old.csv'
 
+    file = open(path + file_name, encoding='utf-8')
+    #读取json文件
+    data = json.load(file)
+    data = Series(data)
+    index_new = []
+    list = []
+    for index in data.index:
+        index_new.append(int(index))
+    data = DataFrame(data, columns=['addr'])
+    data['id'] = index_new
+
+    province = data[data['id']%10000==0]
+    province['addr_new'] = 'NULL'
+    index_item = province[(province['addr'].str.contains('省'))| (province['addr'].str.contains('市')) ].index
+    for item_1 in index_item:
+        province.ix[item_1, 'addr_new'] = province.ix[item_1,'addr'][:-1]
+    province.ix['150000', 'addr_new'] = '内蒙'
+    province.ix['450000', 'addr_new'] = '广西'
+    province.ix['540000', 'addr_new'] = '西藏'
+    province.ix['640000', 'addr_new'] = '宁夏'
+    province.ix['650000', 'addr_new'] = '新疆'
+    province.ix['810000', 'addr_new'] = '香港'
+    province.ix['820000', 'addr_new'] = '澳门'
+    # print(province)
+    # city = data[(data['id']%100==0) & (data['id']%10000!=0)]
+
+
+    #data_2为原地址表，是处理的数据
+    data_old = pd.read_table(path + file_name_2, encoding='utf-8', sep='\t')
+    data_old['call_addr_new'] = "NULL"
+    #flag判断地址字符中是否含有多个省名,如果只有一个，就确定它属于哪个省
+    data_old['flag'] = 0
+    for item_3 in province.index:
+        province_word = province.ix[item_3, 'addr_new']
+        #找到含有该省名的地址，比如 海南
+        item_3_province = data_old[data_old["call_addr"].str.contains(province_word)].index
+        data_old.ix[index, 'flag'] = data_old.ix[index, 'flag'] + 1
+        for item_4 in item_3_province:
+            if data_old.ix[item_4 , 'flag'] == 1:
+                data_old.ix[item_4, 'call_addr']
+            elif data_old.ix[item_4 , 'flag'] > 1:
+                pass
+            else: 
+                pass
+
+
+        data_old.ix[item_3_province, ['call_addr_new']] = province.ix[item_3, 'addr']
+
+    print(data_old)
+
+    for item_2 in data_old.index:
+        addr_old = data_old.ix[item_2, 'call_addr']
+        #先看看地址中是否含有中文，不含有中文，新地址为“未知”
+        if re.search(r'[\u4e00-\u9fa5]', addr_old):
+            pass
+        else:
+            data_old.ix[item_2, 'call_addr_new'] = '未知'
+
+    # print(data_old)
+    #增加新列"call_addr_new"
+    data_old['call_addr_new'] = "NULL"
+
+
+    # print(data_old)
 
 #修改原通话记录中的call_addr
 def alter_addr():
@@ -100,7 +160,7 @@ def alter_addr():
             group['call_addr'].drop_duplicates(index=False)
 
 
-get_local_data()
+read_local_data()
 
 
 
--- test_yj.py
+++ test_yj.py
@@ -4,37 +4,16 @@ import re
 from pandas import Series, DataFrame
 import pandas as pd
 import time
-def merge_sort(alist):
-    if len(alist) <= 1:
-        print('alist:', alist)
-        return alist
-    # 二分分解
-    num = len(alist)// 2
-    left = merge_sort(alist[:num])
-    print('left:', left)
-    right = merge_sort(alist[num:])
-    print('right:', right)
-    # 合并
-    return merge(left,right)
-
-def merge(left, right):
-    '''合并操作，将两个有序数组left[]和right[]合并成一个大的有序数组'''
-    #left与right的下标指针
-    l, r = 0, 0
-    result = []
-    while l<len(left) and r<len(right):
-        if left[l] < right[r]:
-            result.append(left[l])
-            l += 1
-        else:
-            result.append(right[r])
-            r += 1
-    #第一遍时，left的序列l并没有加1，仍然为0
-    result += left[l:]
-    result += right[r:]
-    print('----------------result:', result)
-    return result
-
-alist = [54,26,93,17,77,31,44,55,20]
-sorted_alist = merge_sort(alist)
-print(sorted_alist)
+data= DataFrame({'a':[1, 2, 3, 4],
+                 'b': [6, 7, 8, 9],
+                 'c': ['adkkda', 6, 5, 6]},index=['a', 'b', 'c', 'd'])
+string = '6984国252中'
+data.ix[:2, 'a'] = data.ix[:2, 'a']+1
+print(data)
+# data.ix[0, 'a'] = 555
+# data['d'] = 'NULL'
+# data.ix[0,'c'] = data.ix[0, 'c'][:3]
+# # data.ix[0,'a'] = data.ix[0,'a'][0] + 300
+# ind = data[data['a']>3].index
+# for i in ind:
+#     print(data.ix[i])
