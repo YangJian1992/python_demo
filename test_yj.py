474f46d8310a50068321816bcd885cfb10e8f2c2
YangJian1992 <1556492839@qq.com>
2017-11-27 14:34:44 +0800

    python语法糖

--- test_yj.py
+++ test_yj.py
@@ -1,5 +1,6 @@
 #coding:utf-8
 import os
+import sys
 import re
 import smtplib
 from pandas import Series, DataFrame
@@ -10,20 +11,26 @@ import time
 import datetime
 import threading
 import webbrowser
-import PyPDF2
+from collections import  Iterator
 data= DataFrame({'a':[469025, 2, 3, 4],
                  'b': [6, 7, 8, 9],
                  'c': ['adkkda', 6, 5, 6]},index=['a', 'b', 'c', 'd'])
 
-start = time.time()
-def dfun2():
-    start2 = time.time()
-    df2 = pd.read_table(path + filename_2)
-    print(df2)
-    print('df2的时间是%d----------------------------------'%(time.time()-start2))
-path = 'D:\\work\\database\\ddress_book_rules\\data_code\\test_liuzhibo\\'
-filename = 'call_history_three_months.csv'
-filename_2 = 'call_history_new_1.csv'
+def abc(fun):
+    def de():
+        start = time.time()
+        fun(2, 1, 2, 3, x=4, y=3)
+        print('函数执行所需时间为：%s'%(time.time()-start))
+    return de
+
+@abc
+def gg(x, *args, **kwargs):
+    print(type(args))
+    print(x, args, kwargs)
+    return 'python'
+
+print(gg(2, 1, 2, 3, x=4, y=3))
+
 # thread_obj = threading.Thread(target=dfun2)
 # thread_obj.start()
 # start3 = time.time()
@@ -31,7 +38,6 @@ filename_2 = 'call_history_new_1.csv'
 # print(df1)
 # print('df1时间为%d'%(time.time()-start3))
 # print('累计时间为%d'%(time.time()-start))
-dfun2()
 # subject = 'python mail'
 # message['Subject'] = Header(subject, 'utf-8')
 # print(smtp_obj.sendmail('920892845@qq.com', '1556492839@qq.com', message.as_string()))
