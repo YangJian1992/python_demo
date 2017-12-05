b27588a79043195b74de69b5315729110b71446e
YangJian1992 <1556492839@qq.com>
2017-12-05 15:18:17 +0800

    scrapy

--- spider/crawler.test.py
+++ spider/crawler.test.py
@@ -2,6 +2,9 @@ from urllib import request
 import socket
 import sys
 from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import re
+import scrapy
 
 
 #爬虫的小练习
@@ -15,15 +18,13 @@ def spider_test():
     #设置超时
     timeout = 10
     socket.setdefaulttimeout(timeout)  # 设置超时
-    req_obj = request.Request(URL, headers=headers)
-    page = request.urlopen(req_obj)
+    # req_obj = request.Request(URL, headers=headers)
+    page = urlopen(URL)
     #BeautifulSoup()方便你处理html文档
     bs_obj = BeautifulSoup(page)
     # page = page.decode('utf-8')
-    print(bs_obj.findAll('a', {'class':"friend-link"}))
-    for i in bs_obj.findAll('a', {'class':"friend-link"}):
-        print(type(i))
-    print(type(bs_obj))
+    print(bs_obj.findAll('a', {'href' : "http://www.lagou.com/"}))
+    print(bs_obj.findAll('a', href=re.compile('^http://www.lagou.com')))
 
 if __name__ == '__main__':
     spider_test()
