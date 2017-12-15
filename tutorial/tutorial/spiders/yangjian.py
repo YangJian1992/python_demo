5e6b09adfc42468825a764cd821403f5a4773959
YangJian1992 <1556492839@qq.com>
2017-12-15 18:34:03 +0800

    scrapy

--- tutorial/tutorial/spiders/yangjian.py
+++ tutorial/tutorial/spiders/yangjian.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import pandas as pd
 
 class YangjianSpider(scrapy.Spider):
     name = 'yangjian'
     allowed_domains = ['baidu.com']
-    start_urls = ['http://baidu.com/']
+    start_urls = ['http://www.baidu.com/', 'http://scrapy-chs.readthedocs.io/zh_CN/1.0/intro/tutorial.html#id2']
 
     def parse(self, response):
-        title = response.xpath('/html/head/title/text()')
-        with open('a.html','wb') as file:
-            file.write(response.body)
+        title = response.xpath('/html/head/title/text()').extract()
+        with open('1.txt','w') as file:
+            file.write(title[0])
         print(title)
+
+    
