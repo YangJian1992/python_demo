bac3c5c559df075df3b1002b473fa9d74523e210
YangJian1992 <1556492839@qq.com>
2018-05-16 18:31:03 +0800

    sklearn建立用户需求模型

---
+++ loan_demand_model/users_data_set.py
@@ -0,0 +1,147 @@
+import os
+from pandas import Series, DataFrame
+import pandas as pd
+import time
+import pymysql
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn import svm
+from datetime import datetime
+
+#连接到数据库，输入参数为查询语句字符串，用'''表示，第二个参数为列名，返回查询到的DataFrame格式的数据
+def mysql_connection(select_string, columns_add):
+    start = time.time()
+    conn = pymysql.connect(host='rr-bp1jnr76z49y5k9mno.mysql.rds.aliyuncs.com', port=3306, user='qiandaodao',
+                           passwd='qdddba@2017*', db='qiandaodao', charset='utf8')
+    print('已经连接到数据库，请稍候...')
+    cur = conn.cursor()
+    cur.execute(select_string)
+
+    temp = DataFrame(list(cur.fetchall()), columns=columns_add)
+    print('已经查询到数据，正在处理，请稍候...查询花费时间为%ds。' % (time.time() - start))
+    # 提交
+    conn.commit()
+    # 关闭指针对象
+    cur.close()
+    # 关闭连接对象
+    conn.close()
+    return (temp)
+
+
+def read_data():
+    path = 'D:\\work\\贷款需求模型\\'
+    #这些是先申请额度后借款的用户，不包括申请额度之前有过借款记录的用户。
+    select_string_1 = '''
+    SELECT 
+    cao.user_id as '用户ID',
+    if(ucp.sex='男',1,if(ucp.sex='女', 0 , -1)) as '性别',
+	users.create_time AS '注册时间',
+    max(users.logon_time) as '最近登陆时间',
+    MIN(cao.create_time) AS '最早额度时间',
+    max(cao.create_time) AS '最近额度时间',
+    MIN(ulo.create_time) AS '最早借款时间',
+    MAX(ulo.create_time) AS '最近借款时间',
+	max(ulo.overdue_days) AS '最大逾期天数',
+	COUNT(DISTINCT ulo.id) AS '借款订单数量',
+    count(distinct ulo.payee_bank_card) as '收款银行卡的数量',
+    DATEDIFF(MIN(cao.create_time), users.create_time) AS '最早额度时间与注册时间间隔',
+    DATEDIFF(MIN(ulo.create_time), MIN(cao.create_time)) AS '最早借款时间与最早额度时间间隔',
+    DATEDIFF(MAX(ulo.create_time) , MIN(ulo.create_time)) AS '最近借款时间与最早借款时间间隔',
+    DATEDIFF(max(users.logon_time),MAX(ulo.create_time)) AS '最近借款时间与最近登陆时间间隔',
+    DATEDIFF(MAX(ulo.create_time) , MIN(ulo.create_time))/ COUNT(DISTINCT ulo.id) AS '平均借款间隔'
+FROM
+    (SELECT 
+       user_id, create_time, id
+    FROM
+        qiandaodao.credit_apply_orders 
+    WHERE
+        auth_status = 2
+            AND create_time < '2017-12-13') AS cao
+        INNER JOIN
+    (SELECT 
+        user_id, create_time, id, payee_bank_name, overdue_days, payee_bank_card
+    FROM
+        qiandaodao.user_loan_orders 
+    WHERE
+        create_time < '2017-12-13') 
+        AS ulo ON cao.user_id = ulo.user_id
+        LEFT JOIN
+    users ON ulo.user_id = users.id
+    LEFT JOIN 
+	user_credit_profile as ucp on ucp.user_id = cao.user_id
+GROUP BY cao.user_id
+HAVING MIN(cao.create_time) < MIN(ulo.create_time) 
+
+    '''
+    #下面这些是已经申请额度但没有借款的用户
+    select_string_2 = '''
+
+SELECT 
+    cao.user_id AS '用户ID',
+    if(ucp.sex='男',1,if(ucp.sex='女', 0 , -1)) as '性别',
+    users.create_time AS '注册时间',
+    MAX(users.logon_time) AS '最近登陆时间',
+    MIN(cao.create_time) AS '最早额度时间',
+    MAX(cao.create_time) AS '最近额度时间',
+    MIN(ulo.create_time) AS '最早借款时间',
+    MAX(ulo.create_time) AS '最近借款时间',
+    MAX(ulo.overdue_days) AS '最大逾期天数',
+    COUNT(DISTINCT ulo.id) AS '借款订单数量',
+    COUNT(DISTINCT ulo.payee_bank_card) AS '收款银行卡的数量',
+    DATEDIFF(MIN(cao.create_time), users.create_time) AS '最早额度时间与注册时间间隔',
+    DATEDIFF(MIN(ulo.create_time), MIN(cao.create_time)) AS '最早借款时间与最早额度时间间隔',
+    DATEDIFF(MAX(ulo.create_time), MIN(ulo.create_time)) AS '最近借款时间与最早借款时间间隔',
+    DATEDIFF(MAX(users.logon_time), MAX(ulo.create_time)) AS '最近借款时间与最近登陆时间间隔',
+    DATEDIFF(MAX(ulo.create_time), MIN(ulo.create_time)) / COUNT(DISTINCT ulo.id) AS '平均借款间隔'
+FROM
+    (SELECT 
+        user_id, create_time, id
+    FROM
+        qiandaodao.credit_apply_orders
+    WHERE
+        auth_status = 2
+            AND create_time < '2017-12-13') AS cao
+        LEFT JOIN
+    (SELECT 
+        user_id,
+            create_time,
+            id,
+            payee_bank_name,
+            overdue_days,
+            payee_bank_card
+    FROM
+        qiandaodao.user_loan_orders
+    WHERE
+        create_time < '2017-12-13') AS ulo 
+        ON cao.user_id = ulo.user_id
+        LEFT JOIN
+    users ON cao.user_id = users.id
+        LEFT JOIN
+    user_credit_profile AS ucp ON ucp.user_id = cao.user_id
+WHERE
+    ulo.user_id IS NULL
+GROUP BY cao.user_id
+
+    '''
+    col_names = '用户ID, 性别, 注册时间, 最近登陆时间, 最早额度时间, 最近额度时间, 最早借款时间, 最近借款时间, 最大逾期天数, 借款订单数量, 收款银行卡的数量, 最早额度时间与注册时间间隔, 最早借款时间与最早额度时间间隔, 最近借款时间与最早借款时间间隔, 最近借款时间与最近登陆时间间隔, 平均借款间隔'
+    columns_add = col_names.split(sep=',')
+    columns_add = [x.strip() for x in columns_add]
+    data_1 = mysql_connection(select_string_1, columns_add)
+    data_1['最近借款时间与最近登陆时间间隔'][data_1['最近借款时间与最近登陆时间间隔'].isnull()] = 0
+    data_1['最早额度时间与注册时间间隔'][data_1['最早额度时间与注册时间间隔'].isnull()] = 0
+    #data_2 = mysql_connection(select_string_2, columns_add)
+    data_1_x = data_1[['性别','最大逾期天数', '收款银行卡的数量', '最早额度时间与注册时间间隔', '最早借款时间与最早额度时间间隔', '最近借款时间与最早借款时间间隔', '最近借款时间与最近登陆时间间隔', '平均借款间隔']]
+    #对字段值进行分类，全部转化成离散型变量
+    data_1_y = pd.cut(data_1['借款订单数量'], [1,2, 6, 11, 1000], right=False, labels=[1, 2, 3, 4])
+    data_1_x['最大逾期天数'] = pd.cut(data_1_x['最大逾期天数'], [0, 1, 30, 1000000], right=False, labels=[0, 1, 2])
+    data_1_x['收款银行卡的数量'] = pd.cut(data_1_x['收款银行卡的数量'], [1, 2, 1000], right=False, labels=[1,2])
+    data_1_x['最早额度时间与注册时间间隔'] = pd.cut(data_1_x['最早额度时间与注册时间间隔'], [0, 1, 4, 15, 1000000], right=False, labels=[0, 1, 2, 3])
+    data_1_x['最早借款时间与最早额度时间间隔'] = pd.cut(data_1_x['最早借款时间与最早额度时间间隔'], [0, 1, 4, 15, 1000000], right=False, labels=[0, 1, 2, 3])
+    data_1_x['最近借款时间与最早借款时间间隔'] = pd.cut(data_1_x['最近借款时间与最早借款时间间隔'], [0, 1, 32, 180, 1000000], right=False, labels=[0, 1, 2, 3])
+    data_1_x['最近借款时间与最近登陆时间间隔'] = pd.cut(data_1_x['最近借款时间与最近登陆时间间隔'], [-100, 1, 32, 180, 1000000], right=False,labels=[0, 1, 2, 3])
+    data_1_x['平均借款间隔'] = pd.cut(data_1_x['平均借款间隔'], [0, 1, 32, 1000000], right=False,labels=[0, 1, 2])
+    x_train, x_test, y_train, y_test = train_test_split(data_1_x, data_1_y, test_size=0.3)
+    clf = svm.SVC(kernel='rbf')
+    scores = cross_val_score(clf, data_1_x, data_1_y, cv=5)
+    clf.fit(x_train, y_train)
+    y = clf.predict(x_test)
--- test/test_0_0.py
+++ test/test_0_0.py
@@ -30,36 +30,10 @@ def mysql_connection(select_string, columns_add):
     return (temp)
 
 
-def addr_analysis():
-    select_string = '''
-    select lo.id,e.user_id,d.county,lo.location,ao.location,lo.amount,lo.overdue_days,lo.repay_status from user_loan_orders lo 
-    left join ecshop_orders e on e.id=lo.id
-    left join (select user_id,location from credit_apply_orders where auth_status=2 and auth_time<'2018-01'   group by user_id) ao on ao.user_id=e.user_id
-    inner join user_credit_grant cg on cg.user_id=e.user_id 
-    left join user_credit_profile cp on cp.user_id=e.user_id
-    left join idcard_district d on d.id=left(cp.idcard,6) 
-    where lo.create_time>'2018-01-23' and cg.apply_type=1 and lo.due_repay_date<'2018-04-06';
-    '''
-    columns = ['id', 'user_id', 'county', 'location_1', 'location_2', 'amount', 'overdue_days', 'repay_status']
-    data = mysql_connection(select_string, columns)
-    pattern_1 = r'.*?"province":"(.+?)"|(.*?(?:省|自治区|市|特别行政区))'
-    pattern_2 = r'.*?"city":"(.+?)"|.*?(?:省|自治区|市|特别行政区)(.*?(?:市|自治州|地区|盟|区))'
-    temp_1_1 = data.location_1.str.extract(pattern_1)
-    temp_1_2 = data.location_1.str.extract(pattern_2)
-    temp_2_1 = data.location_2.str.extract(pattern_1)
-    temp_2_2 = data.location_2.str.extract(pattern_2)
-    def com(data):
-        for i in data[data[0].isnull()].index:
-            data.loc[i, 0] = data.loc[i, 1]
-        return data[0]
-    data.insert(4, 'location_1_result',  com(temp_1_1) + com(temp_1_2))
-    data.insert(6, 'location_2_result',  com(temp_2_1) + com(temp_2_2))
-    writer = pd.ExcelWriter('D:\\work\\Desktop\\addr.xlsx')
-    data.to_excel(writer, 'sheet1', encoding='utf-8')
-    writer.save()
 
-def test(select_string, name):
-    columns_add = ['amount', 'location']
+
+def addr(select_string, name):
+    columns_add = ['id', 'amount', 'location']
     data = mysql_connection(select_string, columns_add)
     data['province'] = 'NULL'
     data['city'] = 'NULL'
@@ -68,7 +42,7 @@ def test(select_string, name):
     data_1 = data[(data['location'].notnull()) & (data['location'].str.contains('location'))]
     pattern_1 = r'.*location":"(.*?(?:自治区|省|市))(.*?(?:盟|自治州|地区|市|区|县|自治县)).*'
     data_pc_1 = data_1['location'].str.extract(pattern_1)
-    pattern_2 = r'.*?(.*?(?:自治区|省|市))(.*?(?:盟|自治州|地区|市|区|县|自治县)).*'
+    pattern_2 = r'.*?(.*?(?:自治区|省|市))\|?(.*?(?:盟|自治州|地区|市|区|县|自治县)).*'
     data_2 = data[(data['location'].notnull()) ^ ((data['location'].str.contains('location')))]
     data_pc_2 = data_2['location'].str.extract(pattern_2)
     data_pc = pd.concat([data_pc_1, data_pc_2], ignore_index=False)
@@ -78,34 +52,32 @@ def test(select_string, name):
     data = data.fillna('NULL')
     in_city = data[data['province'].map(lambda x : '市' in x)].index
     data.ix[in_city, 'city'] = data.ix[in_city, 'province']
-    data['pc'] = data['province'] + data['city']
-    result = data.groupby(['province', 'city'])['amount'].sum()
-    pd.ExcelWriter('')
-    data_list = []
 
+
+    data_list = []
     for (k_1, k_2), group in data.groupby(['province', 'city']):
         data_dic = {}
         data_dic['province'] = k_1
         data_dic['city'] = k_2
         data_dic['amount'] = float(group['amount'].sum())
+        data_dic['orders_count'] = len(group)
+        data_dic['aver_amount'] = round(float(group['amount'].sum()) / len(group), 2)
         data_list.append(data_dic)
-    a = pd.DataFrame(data_list)
-    a = a.insert(2, 'amount', a.pop('amount'))
-    a.sort_values(by=['amount'], inplace=True, ascending=False)
+    a = pd.DataFrame(data_list, columns=['province', 'city', 'amount', 'orders_count', 'aver_amount'])
     writer = pd.ExcelWriter('D:\\work\\' + name)
-    a.to_excel()
+    a.to_excel(writer, index=False)
     writer.save()
 
 
 
 if __name__ == "__main__":
     select_string_1 = '''
-    select lo.amount, lo.location from user_loan_orders lo where lo.loan_status=2;
+    select id, lo.amount, lo.location from user_loan_orders lo where lo.loan_status=2;
     '''
     name_1 = 'user_loan_orders.xlsx'
-    test(select_string_1, name_1)
+    addr(select_string_1, name_1)
     select_string_2 = '''
-    select e.total_price-e.down_pay, e.location from ecshop_orders e where e.create_time<'2018-01' and e.order_status in (4,5,10,11);
+    select e.id, e.total_price-e.down_pay, e.location from ecshop_orders e where e.create_time<'2018-01' and e.order_status in (4,5,10,11);
     '''
     name_2= 'ecshop_orders.xlsx'
-    test(select_string_2, name_2)
+    addr(select_string_2, name_2)
